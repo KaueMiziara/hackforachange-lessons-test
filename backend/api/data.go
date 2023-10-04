07fb599a4ed2416d34f499c5203b0621fdfbca30
Kaue Miziara <kauemiziara@gmail.com>
2023-10-04 11:39:19 -0300

    Fix ReadFile filepath in data.go && Add JsonData struct to models

--- backend/api/data.go
+++ backend/api/data.go
@@ -4,19 +4,18 @@ import (
 	"encoding/json"
 	"hackforachange-demo-backend/models"
 	"os"
-	"path/filepath"
 )
 
 func LoadGrades() ([]models.Grade, error) {
-	data, err := os.ReadFile(filepath.Join("data", "database.json"))
+	data, err := os.ReadFile("../data/database.json")
 	if err != nil {
 		return nil, err
 	}
 
-	var grades []models.Grade
-	if err := json.Unmarshal(data, &grades); err != nil {
+	var jsonData models.JsonData
+	if err := json.Unmarshal(data, &jsonData); err != nil {
 		return nil, err
 	}
 
-	return grades, nil
+	return jsonData.Grades, nil
 }
--- backend/models/models.go
+++ backend/models/models.go
@@ -1,5 +1,9 @@
 package models
 
+type JsonData struct {
+	Grades []Grade `json:"grades"`
+}
+
 type Grade struct {
 	ID       int       `json:"id"`
 	Name     string    `json:"name"`
