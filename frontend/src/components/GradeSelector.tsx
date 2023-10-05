6c28017bda6008afed79a7daf0b13c11ef5008c2
Kaue Miziara <kauemiziara@gmail.com>
2023-10-04 23:25:35 -0300

    Update GradeSelector component to fetch grades from backend's API

--- frontend/src/components/GradeSelector.tsx
+++ frontend/src/components/GradeSelector.tsx
@@ -1,14 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ListItem from "./ListItem";
 import "./GradeSelector.css";
 
+interface Grade {
+    id: number;
+    name: string;
+}
 
 const GradeSelector: React.FC = () => {
+    const [grades, setGrades] = useState<Grade[]>([]);
+
+    useEffect(() => {
+        fetch("http://localhost:8080/api/grades")
+            .then((response) => response.json())
+            .then((data) => setGrades(data))
+            .catch((error) => console.error("Error fetching grades: ", error))
+    }, []);
+
     return (
         <div >
             <h2>Select a Grade</h2>
             <div className="grades-container">
-                
+                {grades.map((grade) => (
+                    <ListItem key={grade.id} name={grade.name} />
+                ))}
             </div>
         </div>
     );
