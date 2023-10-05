00d17cd7ccf903a17d29136e0d80c5cebb88f8a6
Kaue Miziara <kauemiziara@gmail.com>
2023-10-05 18:16:13 -0300

    Add LessonsAndExercises routing

--- frontend/src/routing/RoutesConfig.tsx
+++ frontend/src/routing/RoutesConfig.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import MainScreen from "../components/MainScreen";
+import LessonsAndExercisesScreen from "../components/LessonsAndExercisesScreen";
 import SubjectSelection from "../components/SubjectSelection";
 
 const RoutesConfig: React.FC = () => {
@@ -9,6 +10,10 @@ const RoutesConfig: React.FC = () => {
             <Routes>
                 <Route path="/" element={<MainScreen />} />
                 <Route path="/subjects/:gradeID" element={<SubjectSelection />}/>
+                <Route
+                 path="/lessons-and-exercises/:gradeID/:subjectID" 
+                 element={<LessonsAndExercisesScreen />} 
+                />
             </Routes>
         </Router>
     )
