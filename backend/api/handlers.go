dddb4f60e24217a78516c79ed07b332e203a9385
Kaue Miziara <kauemiziara@gmail.com>
2023-10-05 14:16:34 -0300

    Create GetLessonsByGradeAndSubject handler function to get subjects from database && Create its route

--- backend/api/handlers.go
+++ backend/api/handlers.go
@@ -27,7 +27,7 @@ func GetSubjectByGrade(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	subjects, err := GetSubjectByGradeId(gradeID)
+	subjects, err := LoadSubjectByGradeId(gradeID)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
@@ -35,4 +35,27 @@ func GetSubjectByGrade(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(subjects)
+}
+
+func GetLessonsByGradeAndSubject(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	gradeID, err := strconv.Atoi(vars["gradeID"])
+	if err != nil {
+		http.Error(w, "Invalid grade ID", http.StatusBadRequest)
+		return
+	}
+
+	subjectID, err := strconv.Atoi(vars["subjectID"])
+	if err != nil {
+		http.Error(w, "Invalud subject ID", http.StatusBadRequest)
+		return
+	}
+
+	lessons, err := LoadLessonsByGradeAndSubject(gradeID, subjectID)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(lessons)
 }
--- backend/api/routes.go
+++ backend/api/routes.go
@@ -5,4 +5,5 @@ import "github.com/gorilla/mux"
 func SetupRoutes(router *mux.Router) {
 	router.HandleFunc("/api/grades", GetGrades).Methods("GET")
 	router.HandleFunc("/api/subjects/{gradeID}", GetSubjectByGrade).Methods("GET")
+	router.HandleFunc("/api/lessons/{gradeID}/{subjectID}", GetLessonsByGradeAndSubject).Methods("GET")
 }
